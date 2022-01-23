afce7498fb9ce2ffc3daa0eec048e4f5ce4674fa
Trim21 <trim21.me@gmail.com>
2022-01-23 16:37:15 +0800

    disable http logger

--- web/new.go
+++ web/new.go
@@ -1,13 +1,7 @@
 package web
 
 import (
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
-	fiberLogger "github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/hekmon/transmissionrpc/v2"
 
 	"app/pkg/errgo"
@@ -31,29 +25,8 @@ func New(tr *transmissionrpc.Client) (S, error) {
 		GETOnly:               false,
 	})
 
-	app.Use(fiberLogger.New(fiberLogger.Config{
-		Format:       _format(),
-		TimeFormat:   time.RFC3339,
-		TimeInterval: time.Second,
-		Output:       os.Stdout,
-	}))
-
 	handler.SetupRouter(tr, app)
 	logger.Info("start serer")
 
 	return S{app}, nil
-}
-
-func _format() string {
-	format := strings.Join([]string{
-		strconv.Quote("time") + `: "${time}"`,
-		(strconv.Quote("status")) + `: ${status}`,
-		(strconv.Quote("method")) + `: "${method}"`,
-		(strconv.Quote("latency")) + `: "${latency}"`,
-		(strconv.Quote("path")) + `: "${path}"`,
-	}, ", ")
-
-	format = "{" + format + "}\n"
-
-	return format
 }
