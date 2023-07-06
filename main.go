7d3bfa441b42a34403fde97fee26a32b971b67ec
Trim21 <trim21.me@gmail.com>
2023-07-06 13:49:46 +0800

    remove web dir

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	// This would automatically load/inject environment variables from a .env file
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
+	"github.com/trim21/errgo"
 
-	"app/web"
+	"app/reporter"
 )
 
 func main() {
-	if err := web.Start(); err != nil {
+	if err := Start(); err != nil {
 		log.Fatal().Msg("failed to start HTTP server")
 	}
+}
+
+func Start() error {
+	err := reporter.SetupMetrics()
+	if err != nil {
+		return err
+	}
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	return errgo.Wrap(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), http.DefaultServeMux), "failed to start http server")
 }
